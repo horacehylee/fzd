bf18393cd494ca695f03e12cca4cab5afa254d11
Horace Lee <horacehylee2@gmail.com>
2022-02-12 19:42:58 +0800

    Improve ignorer to support deeply nested strings

--- ignorer/ignorer.go
+++ ignorer/ignorer.go
@@ -8,34 +8,43 @@ import (
 )
 
 type Ignorer struct {
+	lines   []string
 	matcher *gitignore.GitIgnore
 }
 
 var ErrTypeNotSupported = errors.New("type is not supported")
 
-func NewIgnorer(elements []interface{}) (*Ignorer, error) {
-	lines := make([]string, len(elements))
-	for _, element := range elements {
-		fmt.Printf("%v", element)
-		switch elem := element.(type) {
-		case string:
-			lines = append(lines, elem)
-		case []string:
-			lines = append(lines, elem...)
-		case []interface{}:
-			strings := make([]string, len(elem))
-			for i, e := range elem {
-				strings[i] = fmt.Sprint(e)
-			}
-			lines = append(lines, strings...)
-		default:
-			return nil, fmt.Errorf("%T %w, only string, []string or []interface{}", element, ErrTypeNotSupported)
+func NewIgnorer(elements ...interface{}) (*Ignorer, error) {
+	i := &Ignorer{
+		lines: make([]string, len(elements)),
+	}
+	for _, elem := range elements {
+		err := i.resolveElement(elem)
+		if err != nil {
+			return nil, err
 		}
 	}
-	matcher := gitignore.CompileIgnoreLines(lines...)
-	return &Ignorer{
-		matcher: matcher,
-	}, nil
+	i.matcher = gitignore.CompileIgnoreLines(i.lines...)
+	return i, nil
+}
+
+func (i *Ignorer) resolveElement(v interface{}) error {
+	switch t := v.(type) {
+	case string:
+		i.lines = append(i.lines, t)
+	case []string:
+		i.lines = append(i.lines, t...)
+	case []interface{}:
+		for _, tt := range t {
+			err := i.resolveElement(tt)
+			if err != nil {
+				return err
+			}
+		}
+	default:
+		return fmt.Errorf("%T %w, only string, []string or []interface{}", t, ErrTypeNotSupported)
+	}
+	return nil
 }
 
 func (i *Ignorer) MatchesPath(path string) bool {
--- ignorer/ignorer_test.go
+++ ignorer/ignorer_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func newIgnorer(t *testing.T, values ...interface{}) *ignorer.Ignorer {
-	ignorer, err := ignorer.NewIgnorer(values)
+	ignorer, err := ignorer.NewIgnorer(values...)
 	assert.NoError(t, err)
 	return ignorer
 }
@@ -51,17 +51,55 @@ func TestNewIgnorerCanCombineSlices(t *testing.T) {
 	assert.False(t, ignorer.MatchesPath("./abc/testing/moreImportant/some_files"))
 }
 
+func TestNewIgnorerWithDeeplyNestedSlices(t *testing.T) {
+	ignorer := newIgnorer(t,
+		"testing",
+		[]interface{}{
+			"!moreImportant",
+			[]interface{}{
+				"!important",
+			},
+		},
+	)
+
+	assert.False(t, ignorer.MatchesPath("./abc/testing/important"), "")
+	assert.False(t, ignorer.MatchesPath("./abc/testing/important/some_files"))
+	assert.False(t, ignorer.MatchesPath("./abc/testing/moreImportant/some_files"))
+}
+
 func TestReturnErrorIfTypeNotStringRelated(t *testing.T) {
 	var err error
-	_, err = ignorer.NewIgnorer([]interface{}{123})
+	_, err = ignorer.NewIgnorer(123)
 	assert.EqualError(t, err, "int type is not supported, only string, []string or []interface{}")
 	assert.True(t, errors.Is(err, ignorer.ErrTypeNotSupported))
 
-	_, err = ignorer.NewIgnorer([]interface{}{true})
+	_, err = ignorer.NewIgnorer(true)
 	assert.EqualError(t, err, "bool type is not supported, only string, []string or []interface{}")
 	assert.True(t, errors.Is(err, ignorer.ErrTypeNotSupported))
 
-	_, err = ignorer.NewIgnorer([]interface{}{[]int{123}})
+	_, err = ignorer.NewIgnorer([]int{123})
 	assert.EqualError(t, err, "[]int type is not supported, only string, []string or []interface{}")
+	assert.True(t, errors.Is(err, ignorer.ErrTypeNotSupported))
+
+	_, err = ignorer.NewIgnorer(
+		[]interface{}{
+			"123",
+			[]interface{}{
+				123,
+			},
+		},
+	)
+	assert.EqualError(t, err, "int type is not supported, only string, []string or []interface{}")
+	assert.True(t, errors.Is(err, ignorer.ErrTypeNotSupported))
+
+	_, err = ignorer.NewIgnorer(
+		[]interface{}{
+			123,
+			[]interface{}{
+				"123",
+			},
+		},
+	)
+	assert.EqualError(t, err, "int type is not supported, only string, []string or []interface{}")
 	assert.True(t, errors.Is(err, ignorer.ErrTypeNotSupported))
 }
