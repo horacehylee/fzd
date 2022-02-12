770051ac0e449c52ed6869cc3b03ef54a1e6713d
Horace Lee <horacehylee2@gmail.com>
2022-02-12 19:45:04 +0800

    Update doc for full imported module name

--- ignorer/doc.go
+++ ignorer/doc.go
@@ -1,2 +1,2 @@
-// Ignorer package for fzd, which wraps sabhiram/go-gitignore package for support of nested structure for gitignore styled strings
+// Ignorer package for fzd, which wraps github.com/sabhiram/go-gitignore package for support of nested structure for gitignore styled strings
 package ignorer
--- walker/doc.go
+++ walker/doc.go
@@ -1,2 +1,2 @@
-// Walker package for fzd, which wraps godirwalk package for more similar interface as filepath.Walk
+// Walker package for fzd, which wraps github.com/karrick/godirwalk package for more similar interface as filepath.Walk
 package walker
